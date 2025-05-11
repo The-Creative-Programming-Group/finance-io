4ed32ffd4ce11e8a824e6090c8b3470fb0dbedd0
FleetAdmiralJakob <jakob.roessner@outlook.de>
2025-05-11 12:57:10 +0200

    setup trpc and api routes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,12 @@
+import { Suspense } from "react";
 import { Text, View } from "react-native";
+import { api } from "~/trpc/react";
+
+export function Hello() {
+  const [hello] = api.post.hello.useSuspenseQuery({ text: "Murtaza" });
+
+  return <Text className="text-red-500">{hello.greeting}</Text>;
+}
 
 export default function Index() {
   return (
@@ -6,6 +14,9 @@ export default function Index() {
       <Text className="text-lg font-bold">
         Edit app/index.tsx to edit this screen. hello fanius
       </Text>
+      <Suspense fallback={<Text>Loading...</Text>}>
+        <Hello />
+      </Suspense>
     </View>
   );
 }
