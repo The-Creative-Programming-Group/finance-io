b3a0cf9a723127c4a12566a01411ec56edbc1605
Murtaza Noori <65132706+murtazanoori@users.noreply.github.com>
2025-06-01 22:59:12 +0430

    fixed errors related to me

--- metro.config.js
+++ metro.config.js
@@ -5,4 +5,11 @@ const { withNativeWind } = require("nativewind/metro");
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+// Add your custom resolver options to the config object
+config.resolver.unstable_enablePackageExports = true;
+config.resolver.unstable_conditionNames = [
+  "browser",
+  ...(config.resolver.unstable_conditionNames || []),
+];
+
 module.exports = withNativeWind(config, { input: "./src/global.css" });
