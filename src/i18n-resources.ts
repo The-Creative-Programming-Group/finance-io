d0bb422be925bc23bc17e30e7217851d68c7c768
FleetAdmiralJakob <jakob.roessner@outlook.de>
2025-10-19 12:18:27 +0200

    fix: reviews from previous PRs
    - made i18n async for better performance

--- src/i18n-resources.ts
+++ src/i18n-resources.ts
@@ -86,25 +86,8 @@ export const resources = {
   },
 };
 
-export const languages = [
-  "ar",
-  "de",
-  "en",
-  "es",
-  "fa",
-  "fr",
-  "hi",
-  "it",
-  "ja",
-  "ko",
-  "ps",
-  "pt",
-  "ru",
-  "tr",
-  "ur",
-  "zh",
-] as const;
-export const namespaces = ["common", "settings"] as const;
+export const languages = ["ar","de","en","es","fa","fr","hi","it","ja","ko","ps","pt","ru","tr","ur","zh"] as const;
+export const namespaces = ["common","settings"] as const;
 
 export type LanguageKey = (typeof languages)[number];
 export type Namespace = (typeof namespaces)[number];
